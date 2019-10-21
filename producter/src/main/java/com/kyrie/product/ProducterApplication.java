fbc4f5a238e0cce0610a671f3a8677c119386558
tengxiao.ma <tengxiao.ma@huifu.com>
2019-10-21 18:08:10 +0800

    删除多余代码

--- producter/src/main/java/com/kyrie/product/ProducterApplication.java
+++ producter/src/main/java/com/kyrie/product/ProducterApplication.java
@@ -2,7 +2,6 @@ package com.kyrie.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @ClassName com.kyrie.product.ProducterApplication
@@ -11,7 +10,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  * @Date 2019/10/21 15:23
  **/
 @SpringBootApplication
-@EnableScheduling
 public class ProducterApplication {
 
     public static void main(String[] args) {
